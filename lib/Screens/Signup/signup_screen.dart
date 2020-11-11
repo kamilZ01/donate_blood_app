bedbc6b80e2db0d89cc895f5703dba9edc9daa3b
kamilZ01 <224546@edu.p.lodz.pl>
2020-11-11 01:37:29 +0100

    designed SignUp Page and created it.

---
+++ lib/Screens/Signup/signup_screen.dart
@@ -0,0 +1,11 @@
+import 'package:donate_blood/Screens/Signup/components/body.dart';
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
