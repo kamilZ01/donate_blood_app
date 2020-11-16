62532b04d67c5a7d5e7e40ea867cdb0b176797d1
kamilZ01 <224546@edu.p.lodz.pl>
2020-11-16 19:50:38 +0100

    changed design of User Profile Detail screen, designed User Settings screen, added 'settings' button in profile tab, which allows you to change user settings

---
+++ lib/Screens/ProfileDetails/components/settings_page.dart
@@ -0,0 +1,187 @@
+import 'package:donate_blood/constants.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 1,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_outlined,
+            color: kPrimaryColor,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+        child: ListView(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Settings",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              children: [
+                Text(
+                  "Account",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Divider(
+              height: 15,
+              thickness: 2,
+            ),
+            buildAccountOptionRow(
+                context, "Change password", Icon(Icons.vpn_key)),
+            buildAccountOptionRow(
+                context, "Language", Icon(Icons.language_outlined)),
+            buildAccountOptionRow(context, "Privacy and security",
+                Icon(Icons.lock_outline_rounded)),
+            buildAccountOptionRow(context, "About", Icon(Icons.info)),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Text(
+                  "Notifications",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Divider(
+              height: 15,
+              thickness: 2,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            buildNotificationOptionRow("Urgent blood donation", true),
+            buildNotificationOptionRow("Account activity", false),
+            SizedBox(
+              height: 50,
+            ),
+            Center(
+              child: RaisedButton(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                onPressed: () {},
+                color: Colors.red,
+                child: Text("SIGN OUT",
+                    style: TextStyle(
+                      fontSize: 16,
+                      letterSpacing: 2.2,
+                      color: Colors.white,
+                    )),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row buildNotificationOptionRow(String title, bool isActive) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey[600]),
+        ),
+        Transform.scale(
+          scale: 0.7,
+          child: CupertinoSwitch(
+            trackColor: kPrimaryColor,
+            value: isActive,
+            onChanged: (bool val) {},
+          ),
+        ),
+      ],
+    );
+  }
+
+  GestureDetector buildAccountOptionRow(
+      BuildContext context, String title, Icon icona) {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text(title),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text("Option 1"),
+                ],
+              ),
+              actions: [
+                FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    "Close",
+                    style: TextStyle(
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 0.1),
+        child: Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              icon: icona,
+            ),
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey[450],
+              ),
+            ),
+            Expanded(child: SizedBox()),
+            Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.grey,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/Screens/ProfileDetails/profile_details_screen.dart
+++ lib/Screens/ProfileDetails/profile_details_screen.dart
@@ -1,19 +1,21 @@
 import 'package:donate_blood/Screens/HomeUserPage/home_page_screen.dart';
-import 'package:donate_blood/Screens/Login/login_screen.dart';
-import 'package:donate_blood/Screens/ProfileDetails/components/user_detail.dart';
+import 'package:donate_blood/Screens/ProfileDetails/components/settings_page.dart';
 import 'package:donate_blood/components/header_curved_container.dart';
 import 'package:donate_blood/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class ProfilePage extends StatelessWidget {
+  bool showPassword = false;
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
-        elevation: 0.0,
+        elevation: 1,
         backgroundColor: kPrimaryColor,
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios),
@@ -24,6 +26,24 @@ class ProfilePage extends StatelessWidget {
             );
           },
         ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.settings_outlined,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return SettingsPage();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         // physics: BouncingScrollPhysics(),
@@ -41,11 +61,11 @@ class ProfilePage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Padding(
-                  padding: EdgeInsets.all(20.0),
+                  padding: EdgeInsets.all(10.0),
                   child: Text(
-                    "Profile",
+                    "Edit Profile",
                     style: TextStyle(
-                      fontSize: 35,
+                      fontSize: 30,
                       letterSpacing: 1.5,
                       color: Colors.white,
                       fontWeight: FontWeight.w600,
@@ -71,36 +91,121 @@ class ProfilePage extends StatelessWidget {
                         ),
                       ),
                       Positioned(
-                        left: width * 0.39,
-                        top: height * 0.20,
-                        child: CircleAvatar(
-                          backgroundColor: Colors.black54,
-                          child: IconButton(
-                            icon: Icon(
-                              Icons.edit,
-                              color: Colors.white,
+                        // left: width * 0.39,
+                        // top: height * 0.20,
+                        bottom: 20,
+                        right: 0,
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 4,
+                              color: Theme.of(context).scaffoldBackgroundColor,
                             ),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) {
-                                    return LoginScreen();
-                                  },
-                                ),
-                              );
-                            },
+                            color: Colors.grey,
+                          ),
+                          child: Icon(
+                            Icons.edit,
+                            color: Colors.white,
                           ),
                         ),
                       ),
                     ],
                   ),
                 ),
+                SizedBox(
+                  height: 20,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(
+                        left: 25,
+                        right: 25,
+                      ),
+                      child: Column(
+                        children: [
+                          buildTextField("Full Name", "Jan Kowalski", false),
+                          buildTextField(
+                              "E-mail", "jan_kowalski@gmail.com", false),
+                          buildTextField("Phone", "123-456-789", false),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              OutlineButton(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 50,
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                highlightedBorderColor: Colors.white,
+                                onPressed: () {},
+                                child: Text(
+                                  "CANCEL",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    letterSpacing: 2.2,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              RaisedButton(
+                                onPressed: () {},
+                                color: kPrimaryColor,
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 50,
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Text(
+                                  "SAVE",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    letterSpacing: 2.2,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
-            UserDetail(),
-            UserDetail(),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildTextField(
+      String labelText, String placeholder, bool isPasswordTextField) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15.0),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.only(bottom: 3),
+          labelText: labelText,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          hintText: placeholder,
+          hintStyle: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
         ),
       ),
     );
